0cea02e9e69cb62f9d00802850f3e0c47e5b16df
Viktor Zubchik <vzubviktor@gmail.om>
2021-09-09 19:58:18 +0200

    fix bugs

--- resolver/linkDomain_rules.py
+++ resolver/linkDomain_rules.py
@@ -8,16 +8,16 @@ def linkDomain_rules(answer, correct_answer):
 		status = 'Valid'
 		comment = 'Link domain'
 
-	
-		
+	elif 'The DNS' in answer:
+		status = 'Invalid'
+		comment = 'CNAME record not found'
+
 	elif answer != correct_answer:
 		status = 'Invalid'
 		comment = 'Incorrect CNAME record'
 
 	else:
-		status = 'Invalid'
-		comment = 'CNAME record not found'
-
+		pass
 
 	output.append(status)
 	output.append(comment)
