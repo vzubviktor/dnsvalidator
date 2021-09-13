3976ff7ae6ad193b1723cf9059bfe0d3b8fd817a
Viktor Zubchik <vzubviktor@gmail.om>
2021-09-13 10:12:05 +0200

    clean functions and views

--- resolver/myfunctions.py
+++ resolver/myfunctions.py
@@ -1,24 +1,95 @@
-# ### pulls up dns records
-# def get_records(domain):
-# # import dns.resolver
-# 	final_answers=[]
+import dns.resolver
+import dns.reversename
+import csv
+import io
 
-# 	try:
-# 		answers=dns.resolver.resolve(domain, record_val)
-# 		for value in answers:
-# 			final_answers.append(value.to_text())			
-# 	except Exception as e:
-# 		final_answers.append(e)
-# 	return final_answers
+from resolver.mxrules import *
+from resolver.nsrules import *
 
-# ### IF PTR record - processe ip to get pter record
 
-# def get_ptr(address):
-# 	final_answers=[]
-# 	try:
-# 		domain_address = dns.reversename.from_address(address)
-# 		ptr_record = dns.resolver.resolve(domain_address, 'PTR')[0]
-# 		final_answers.append(ptr_record)
-# 	except Exception as e:
-# 		final_answers.append(e)
-# 	return final_answers
+### Function to process csv
+def process_csv(csv_file):
+		domain_list = []
+		csvf = io.TextIOWrapper(csv_file, encoding="utf-8", )
+		reader = csv.reader(csvf, )
+		for value in reader:
+			domain_list.append(value)
+		return flatten(domain_list)
+
+
+## Function to get main records
+
+def get_records(domain,record_val):
+# import dns.resolver
+	final_answers=[]
+
+	try:
+		answers=dns.resolver.resolve(domain, record_val)
+		for value in answers:
+			final_answers.append(value.to_text())			
+	except Exception as e:
+		final_answers.append(str(e))
+	return final_answers
+
+
+### Function to get PTR record 
+
+def get_ptr(address):
+		final_answers=[]
+		try:
+			domain_address = dns.reversename.from_address(address)
+			ptr_record = dns.resolver.resolve(domain_address, 'PTR')[0]
+			final_answers.append(ptr_record)
+		except Exception as e:
+			final_answers.append(e)
+		return final_answers
+
+
+### Get chosen  records for domain, applies rules, creates status and comment 
+	### and create objects to be displayed at front end, 
+
+def make_objlist(record_val, domain_list):
+	obj_list = []
+
+	if record_val == 'PTR':
+		for obj in domain_list:
+			record_result = get_ptr(obj)
+			obj_list.append(obj)
+	
+	elif record_val == 'MX':
+		for obj in domain_list:
+			record_result = get_records(obj, record_val)
+			mx_objects = mx_create(record_result)
+			stat_and_comm = mx_rules(mx_objects)
+			obj = Domain( record_name = obj,
+						  record_type= record_val,
+						  record_result =  record_result,
+						  status = stat_and_comm[0],
+						  comment = stat_and_comm[1] )
+			obj_list.append(obj)
+	
+	elif record_val == 'NS':
+		for obj in domain_list:
+			record_result = get_records(obj,record_val)
+			ns_objects = ns_create(record_result)
+			stat_and_comm = ns_rules(ns_objects)
+			obj = Domain( record_name = obj,
+			              record_type= record_val, 
+			              record_result =  record_result, 
+			              status = stat_and_comm[0], 
+			              comment = stat_and_comm[1] )
+			obj_list.append(obj)
+	
+	else:
+		for obj in domain_list:
+			record_result = get_records(obj,record_val)
+			# mx_objects = mx_create(record_result)
+			# stat_and_comm = mx_rules(mx_objects)
+			obj = Domain( record_name = obj,
+			              record_type= record_val,
+			              record_result =  record_result,
+			              status = '',
+			              comment = '' )
+			obj_list.append(obj)
+
+	return obj_list
--- resolver/views.py
+++ resolver/views.py
@@ -1,16 +1,19 @@
+
+# Importing django modules
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.admin.utils import flatten
 from .models import Domain, MXrecord, NSrecord
+# Importing python modules 
 import dns.resolver
 import dns.reversename
-from resolver.mxrules import *
-from resolver.nsrules import *
+# Importing all the functionas that are used in views
+from resolver.myfunctions import *
 from resolver.linkDomain_rules import *
-import csv
-import io
 
-# 	
+
+
+# create views	
 
 def index(request):
 	return render(request, 'index.html')
@@ -22,103 +25,13 @@ def index(request):
 
 def compute(request):
 
-### Gets Raw data from input and cleans for processsing	
-
-	### check box list
-	
-
 	domain_val = request.POST['domains']
 	record_val = request.POST['records']
 	delimitor = request.POST['delimitor']
 	domain_list=domain_val.split(delimitor) # cleaned list of domains 
-    
+	obj_list = make_objlist(record_val, domain_list) ### resolve records for given domain and prepare result for output 
 
-    ### pulls up dns records
-	def get_records(domain):
-	# import dns.resolver
-		final_answers=[]
-
-		try:
-			answers=dns.resolver.resolve(domain, record_val)
-			for value in answers:
-				final_answers.append(value.to_text())			
-		except Exception as e:
-			final_answers.append(e)
-		return final_answers
-
-	### IF PTR record - processe ip to get pter record
-
-	def get_ptr(address):
-		final_answers=[]
-		try:
-			domain_address = dns.reversename.from_address(address)
-			ptr_record = dns.resolver.resolve(domain_address, 'PTR')[0]
-			final_answers.append(ptr_record)
-		except Exception as e:
-			final_answers.append(e)
-		return final_answers
-	
-### finalizes object for output to frontend
-
-	
-	obj_list = []
-
-	### creates objects for PTR record
-
-	if record_val == 'PTR':
-		for obj in domain_list:
-			record_result = get_ptr(obj)
-			 
-			obj_list.append(obj)
-
-
-	### create objects for MX records and applies rules
-	elif record_val == 'MX':
-
-		for obj in domain_list:
-			record_result = get_records(obj)
-			mx_objects = mx_create(record_result)
-			stat_and_comm = mx_rules(mx_objects)
-			obj = Domain( record_name = obj, record_type= record_val, record_result =  record_result, status = stat_and_comm[0], comment = stat_and_comm[1] )
-			obj_list.append(obj)
-
-	### create objects for NS records and applies rules
-
-	elif record_val == 'NS':
-
-		for obj in domain_list:
-			record_result = get_records(obj)
-			ns_objects = ns_create(record_result)
-			stat_and_comm = ns_rules(ns_objects)
-			obj = Domain( record_name = obj, record_type= record_val, record_result =  record_result, status = stat_and_comm[0], comment = stat_and_comm[1] )
-			obj_list.append(obj)
-
-
-	### create objects for domains and records with no rules
-	else:
-		for obj in domain_list:
-			record_result = get_records(obj)
-			# mx_objects = mx_create(record_result)
-			# stat_and_comm = mx_rules(mx_objects)
-			obj = Domain( record_name = obj, record_type= record_val, record_result =  record_result, status = '' , comment = '' )
-			obj_list.append(obj)
-
-
-		
-
-	return render(request, 'result.html', { 
-
-	'record_val' : record_val, 
-		# 'output' : output, 
-	'domain_list' : domain_list, 
-	
-	# 'record_result' : record_result,
-	# 'end_result' : end_result, 
-	'obj_list' : obj_list, 
-	
-	
-
-	 })
+	return render(request, 'result.html', {'obj_list' : obj_list})
 
 
 
@@ -128,87 +41,10 @@ def compute_csv(request):
 	if request.method=='POST':
 		csv_file = request.FILES['csv_file']
 		record_val = request.POST['records']
-
-	def process_csv(csv_file):
-		domain_list = []
-		csvf = io.TextIOWrapper(csv_file, encoding="utf-8", )
-		reader = csv.reader(csvf, )
-		for value in reader:
-			domain_list.append(value)
-		return flatten(domain_list)
-
-	
 	domain_list = process_csv(csv_file)
+	obj_list = make_objlist(record_val, domain_lists)### resolve records for given domain and prepare result for output 
 
-	### pulls up dns records
-	def get_records(domain):
-	# import dns.resolver
-		final_answers=[]
-
-		try:
-			answers=dns.resolver.resolve(domain, record_val)
-			for value in answers:
-				final_answers.append(value.to_text())			
-		except Exception as e:
-			final_answers.append(str(e))
-		return final_answers
-
-	### IF PTR record - processe ip to get pter record
-
-	def get_ptr(address):
-		final_answers=[]
-		try:
-			domain_address = dns.reversename.from_address(address)
-			ptr_record = dns.resolver.resolve(domain_address, 'PTR')[0]
-			final_answers.append(ptr_record)
-		except Exception as e:
-			final_answers.append(str(e))
-		return final_answers
-
-### finalizes object for output to frontend
-	obj_list = []
-
-	### creates objects for PTR record
-
-	if record_val == 'PTR':
-		for obj in domain_list:
-			record_result = get_ptr(obj)
-			obj = Domain( record_name = obj, record_type= record_val, record_result =  record_result, status = '' , comment = '' )
-			obj_list.append(obj)
-
-
-	### create objects for MX records and applies rules
-	elif record_val == 'MX':
-
-		for obj in domain_list:
-			record_result = get_records(obj)
-			mx_objects = mx_create(record_result)
-			stat_and_comm = mx_rules(mx_objects)
-			obj = Domain( record_name = obj, record_type= record_val, record_result =  record_result, status = stat_and_comm[0], comment = stat_and_comm[1] )
-			obj_list.append(obj)
-
-	### create objects for NS records and applies rules
-
-	elif record_val == 'NS':
-
-		for obj in domain_list:
-			record_result = get_records(obj)
-			ns_objects = ns_create(record_result)
-			stat_and_comm = ns_rules(ns_objects)
-			obj = Domain( record_name = obj, record_type= record_val, record_result =  record_result, status = stat_and_comm[0], comment = stat_and_comm[1] )
-			obj_list.append(obj)
-
-
-	### create objects for domains and records with no rules
-	else:
-		for obj in domain_list:
-			record_result = get_records(obj)
-			# mx_objects = mx_create(record_result)
-			# stat_and_comm = mx_rules(mx_objects)
-			obj = Domain( record_name = obj, record_type= record_val, record_result =  record_result, status = '' , comment = '' )
-			obj_list.append(obj)
-
-	
+	### writes csv files with results and output to frontend 
 	response = HttpResponse(content_type = 'text/csv')
 	writer  = csv.writer(response, lineterminator='\n')
 	writer.writerow(['domain' , 'DNS record', 'status' , 'comment'])
@@ -269,11 +105,9 @@ def compute_multi(request):
 			obj = Domain( record_name = domain, record_type= record_val, record_result =  record_result, status = '' , comment = '' )
 			return obj
 
-
 		### create objects for MX records and applies rules
 		elif record_val == 'MX':
 
-			
 			record_result = get_records(domain, record_val)
 			mx_objects = mx_create(record_result)
 			stat_and_comm = mx_rules(mx_objects)
@@ -283,14 +117,12 @@ def compute_multi(request):
 		### create objects for NS records and applies rules
 
 		elif record_val == 'NS':
-
 			
 			record_result = get_records(domain, record_val)
 			ns_objects = ns_create(record_result)
 			stat_and_comm = ns_rules(ns_objects)
 			obj = Domain( record_name = domain, record_type= record_val, record_result =  record_result, status = stat_and_comm[0], comment = stat_and_comm[1] )
 			return obj
-
 
 		### create objects for domains and records with no rules
 		else:
@@ -323,44 +155,21 @@ def linkDomain(request):
 	link_subdomain = request.POST['linkDomain']
 	obj_list = []
 	
-	### checking cname record
-
-	def check_cname_domain(domain):
-	
-		final_answers=[]
-
-		try:
-			answers=dns.resolver.resolve(domain, 'CNAME')
-			for value in answers:
-				final_answers.append(value.to_text())			
-		except Exception as e:
-			final_answers.append(str(e))
-		return final_answers
-
-	### get link domain functions 
-	
-	def correct_link_domain(domain):
-		return domain.replace('.', '-') + '.emarsys.net.'
-
 	### create object for output and applies linkDomain rules 
 
-	link_answer = check_cname_domain(link_subdomain)
-	correct_link_answer = correct_link_domain(link_subdomain)
+	link_answer = get_records(link_subdomain,'CNAME')
+	correct_link_answer = link_subdomain.replace('.', '-') + '.emarsys.net.'
 	stat_and_comm = linkDomain_rules(link_answer, correct_link_answer)
 	obj = Domain( record_name = link_subdomain, record_type= 'CNAME', record_result =  link_answer, status = stat_and_comm[0], comment = stat_and_comm[1] )
 	obj_list.append(obj)
-
 
 
 	###  sender domain functions
 		
 		### create object for output and applies Bounces rules 
 
-	def bounces_sender_domain(domain):
-		return check_cname_domain(bounces_domain)
-
 	bounces_domain  = 'bounces.' + sender_domain
-	bounces_answer = bounces_sender_domain(sender_domain)
+	bounces_answer = get_records(bounces_domain,'CNAME')
 	correct_bounces_answer = 'bounces.emarsys.net.'
 	stat_and_comm = bounces_rules(bounces_answer, correct_bounces_answer)
 	obj = Domain( record_name = bounces_domain, record_type= 'CNAME', record_result =  bounces_answer, status = stat_and_comm[0], comment = stat_and_comm[1] )
@@ -369,11 +178,8 @@ def linkDomain(request):
 
 		### create object for output and applies  key5.dkim Rules
 
-	def key5_sender_domain(domain):
-		return check_cname_domain(key5_domain)
-
 	key5_domain = 'key5._domainkey.' + sender_domain
-	key5_answer = key5_sender_domain(sender_domain)
+	key5_answer = get_records(key5_domain,'CNAME')
 	correct_key5_answer = 'key5.dkim.emarsys.net.'
 	stat_and_comm = key5_rules(key5_answer, correct_key5_answer)
 	obj = Domain( record_name = key5_domain, record_type= 'CNAME', record_result =  key5_answer, status = stat_and_comm[0], comment = stat_and_comm[1] )
@@ -381,11 +187,8 @@ def linkDomain(request):
 
 		### create object for output and applies  key6.dkim Rules
 
-	def key6_sender_domain(domain):
-		return check_cname_domain(key6_domain)
-
 	key6_domain = 'key6._domainkey.' + sender_domain
-	key6_answer = key6_sender_domain(sender_domain)
+	key6_answer = get_records(key6_domain,'CNAME')
 	correct_key6_answer = 'key6.dkim.emarsys.net.'
 	stat_and_comm = key6_rules(key6_answer, correct_key6_answer)
 	obj = Domain( record_name = key6_domain, record_type= 'CNAME', record_result =  key6_answer, status = stat_and_comm[0], comment = stat_and_comm[1] )
@@ -393,64 +196,28 @@ def linkDomain(request):
 
 		### create object for output and applies  mx Rules
 
-	def mx_sender_domain(domain):
-		
 	
-		final_answers=[]
-
-		try:
-			answers=dns.resolver.resolve(domain, 'MX')
-			for value in answers:
-				final_answers.append(value.to_text())			
-		except Exception as e:
-			final_answers.append(str(e))
-		return final_answers
-
-	mx_answer = mx_sender_domain(sender_domain)
+	mx_answer = get_records(sender_domain,'MX')
 	stat_and_comm = mx_sender_rules(mx_answer)
 	obj = Domain( record_name = sender_domain, record_type= 'MX', record_result =  mx_answer, status = stat_and_comm[0], comment = stat_and_comm[1] )
 	obj_list.append(obj)
 
 		### create object for output and applies  TXT Rules
 	
-	def txt_sender_domain(domain):
-		
-	
-		final_answers=[]
-
-		try:
-			answers=dns.resolver.resolve(domain, 'TXT')
-			for value in answers:
-				final_answers.append(value.to_text())			
-		except Exception as e:
-			final_answers.append(str(e))
-		return final_answers
-		
-	txt_answer = txt_sender_domain(sender_domain)
+			
+	txt_answer = get_records(sender_domain,'TXT')
 	stat_and_comm = txt_sender_rules(txt_answer)
 	obj = Domain( record_name = sender_domain, record_type= 'TXT', record_result =  txt_answer, status = stat_and_comm[0], comment = stat_and_comm[1] )
 	obj_list.append(obj)
 
 		### create object for output and applies  DMARC Rules
-
-	def dmarc_sender_domain(domain):
-		return txt_sender_domain(dmarc_domain)
-
+	
 	dmarc_domain = '_dmarc.' + sender_domain
-	dmarc_answer = dmarc_sender_domain(sender_domain)
+	dmarc_answer = get_records(dmarc_domain,'TXT')
 	stat_and_comm = dmarc_sender_rules(dmarc_answer)
 	obj = Domain( record_name = dmarc_domain, record_type= 'DMARC', record_result =  dmarc_answer, status = stat_and_comm[0], comment = stat_and_comm[1] )
 	obj_list.append(obj)
 		
 
-
-
-
-
-
-
-
-
-	
 	return render(request, 'linkDomain.html', {'obj_list' : obj_list})
 
